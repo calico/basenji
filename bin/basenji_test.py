1eca7b4d2fe3699689cc6d0db26a8a50faf36f15
David Kelley <davidkelley44@gmail.com>
2016-07-04 10:45:56 -0400

    peaks AUC

--- bin/basenji_test.py
+++ bin/basenji_test.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
+from __future__ import print_function
 from optparse import OptionParser
 import time
 
 import h5py
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import roc_auc_score
 import tensorflow as tf
 
 import basenji
@@ -18,17 +22,18 @@ import basenji
 # main
 ################################################################################
 def main():
-    usage = 'usage: %prog [options] <model_file> <data_file>'
+    usage = 'usage: %prog [options] <params_file> <model_file> <data_file>'
     parser = OptionParser(usage)
     parser.add_option('-b', dest='batch_size', default=None, type='int', help='Batch size')
-    parser.add_option('-j', '--job', dest='job')
+    parser.add_option('-p', dest='peaks_hdf5', help='Compute AUC for sequence peak calls [Default: %default]')
     (options,args) = parser.parse_args()
 
-    if len(args) != 2:
-    	parser.error('Must provide model and test data HDF5')
+    if len(args) != 3:
+    	parser.error('Must provide paramters, model, and test data HDF5')
     else:
-        model_file = args[0]
-        data_file = args[1]
+        params_file = args[0]
+        model_file = args[1]
+        data_file = args[2]
 
     #######################################################
     # load data
@@ -40,7 +45,7 @@ def main():
     #######################################################
     # model parameters and placeholders
     #######################################################
-    job = basenji.io.read_job_params(options.job)
+    job = basenji.io.read_job_params(params_file)
 
     job['batch_length'] = test_seqs.shape[1]
     job['seq_depth'] = test_seqs.shape[2]
@@ -55,7 +60,7 @@ def main():
         options.batch_size = dr.batch_size
 
     #######################################################
-    # train
+    # test
     #######################################################
     # initialize batcher
     batcher_test = basenji.batcher.Batcher(test_seqs, test_targets, options.batch_size)
@@ -68,11 +73,52 @@ def main():
         saver.restore(sess, model_file)
 
         # test
-        test_loss, test_r2 = dr.test(sess, batcher_test)
+        test_loss, test_r2, test_preds = dr.test(sess, batcher_test, return_preds=True)
 
         # print
         print('Test loss: %7.5f' % test_loss)
         print('Test R2:   %7.5f' % test_r2)
+
+        #######################################################
+        # peaks AUC
+        #######################################################
+        if options.peaks_hdf5:
+            # use validation set to train
+            valid_seqs = data_open['valid_in']
+            valid_targets = data_open['valid_out']
+
+            # initialize batcher
+            batcher_valid = basenji.batcher.Batcher(valid_seqs, valid_targets, options.batch_size)
+
+            # make predictions
+            _, _, valid_preds = dr.test(sess, batcher_valid, return_preds=True)
+            valid_n = valid_preds.shape[0]
+
+            print(valid_preds.shape)
+
+            # load peaks
+            peaks_open = h5py.File(options.peaks_hdf5)
+            valid_peaks = np.array(peaks_open['valid_out'])
+            test_peaks = np.array(peaks_open['test_out'])
+            peaks_open.close()
+
+            # compute target AUCs
+            target_aucs = []
+            for ti in range(test_peaks.shape[1]):
+                # train a predictor for peak calls
+                model = LinearRegression()
+                model.fit(valid_preds[:,:,ti], valid_peaks[:valid_n,ti])
+
+                # predict peaks for test set
+                test_peaks_preds = model.predict(test_preds[:,:,ti])
+                test_n = test_peaks_preds.shape[0]
+
+                # compute AUC
+                auc = roc_auc_score(test_peaks[:test_n,ti], test_peaks_preds)
+                target_aucs.append(auc)
+                print('%d AUC: %f' % (ti,auc))
+
+            print('AUC: %f' % np.mean(target_aucs))
 
     data_open.close()
 

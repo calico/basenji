d841d5eebee9d2d2db1c59523d8f7c09b9be8990
David Kelley <davidkelley44@gmail.com>
2016-07-05 10:20:38 -0400

    init

---
+++ bin/basenji_sat.py
@@ -0,0 +1,434 @@
+#!/usr/bin/env python
+from optparse import OptionParser
+import os
+import random
+import subprocess
+import tempfile
+import time
+
+import h5py
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import seaborn as sns
+import tensorflow as tf
+
+import basenji.io
+from seq_logo import seq_logo
+
+################################################################################
+# basenji_sat.py
+#
+# Perform an in silico saturated mutagenesis of the given test sequences
+# using the given model.
+################################################################################
+
+################################################################################
+# main
+################################################################################
+def main():
+    usage = 'usage: %prog [options] <params_file> <model_file> <input_file>'
+    parser = OptionParser(usage)
+    parser.add_option('-b', dest='batch_size', default=None, type='int', help='Batch size')
+    parser.add_option('-c', dest='cuda', default=False, action='store_true', help='Run on GPGPU [Default: %default]')
+    parser.add_option('-l', dest='satmut_len', default=200, type='int', help='Length of centered sequence to mutate [Default: %default]')
+    parser.add_option('-m', dest='min_limit', default=0.1, type='float', help='Minimum heatmap limit [Default: %default]')
+    parser.add_option('-o', dest='out_dir', default='heat', help='Output directory [Default: %default]')
+    parser.add_option('-r', dest='rng_seed', default=1, type='float', help='Random number generator seed [Default: %default]')
+    parser.add_option('-s', dest='sample', default=None, type='int', help='Sample sequences from the test set [Default:%default]')
+    parser.add_option('-t', dest='targets', default='0', help='Comma-separated list of target indexes to plot (or -1 for all) [Default: %default]')
+    (options,args) = parser.parse_args()
+
+    if len(args) != 3:
+        parser.error('Must provide parameters and model files and input sequences (as a FASTA file or test data in an HDF file')
+    else:
+        params_file = args[0]
+        model_file = args[1]
+        test_file = args[2]
+
+    if not os.path.isdir(options.out_dir):
+        os.mkdir(options.out_dir)
+
+    random.seed(options.rng_seed)
+
+    #################################################################
+    # parse input file
+    #################################################################
+    seqs, seqs_1hot, targets = parse_input(test_file, options.sample)
+
+    seqs_n = seqs_1hot.shape[0]
+
+    #################################################################
+    # setup model
+    #################################################################
+    job = basenji.io.read_job_params(params_file)
+
+    job['batch_length'] = seqs_1hot.shape[1]
+    job['seq_depth'] = seqs_1hot.shape[2]
+    job['num_targets'] = targets.shape[2]
+
+    t0 = time.time()
+    dr = basenji.rnn.RNN()
+    dr.build(job)
+    print('Model building time %f' % (time.time()-t0))
+
+    if options.batch_size is None:
+        options.batch_size = dr.batch_size
+
+
+    #################################################################
+    # predict modifications
+    #################################################################
+    # supplement with saturated mutagenesis
+    sat_seqs_1hot = satmut_seqs(seqs_1hot, options.satmut_len)
+
+    # initialize batcher
+    batcher_sat = basenji.batcher.Batcher(sat_seqs_1hot, batch_size=options.batch_size)
+
+    # initialie saver
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        # load variables into session
+        saver.restore(sess, model_file)
+
+        # predict
+        sat_preds = dr.predict(sess, batcher_sat)
+
+
+    #################################################################
+    # plots
+    #################################################################
+    # decide which targets to plot
+    if options.targets == '-1':
+        plot_targets = range(sat_preds.shape[2])
+    else:
+        plot_targets = [int(ti) for ti in options.targets.split(',')]
+
+    for si in range(seqs_n):
+        ##############################################
+        # compute delta, loss, and gain matrices
+        ##############################################
+        # compute the matrix of prediction deltas: (4 x L_sm x T) array
+        sat_delta = delta_matrix(seqs_1hot, si, sat_preds, options.satmut_len)
+
+        # sat_loss, sat_gain = loss_gain(sat_delta, sat_preds[si], options.satmut_len)
+        sat_loss = sat_delta.min(axis=0)
+        sat_gain = sat_delta.max(axis=0)
+
+        ##############################################
+        # plot targets
+        ##############################################
+        for ti in plot_targets:
+            # setup plot
+            sns.set(style='white', font_scale=1)
+            spp = subplot_params(sat_delta.shape[1])
+            plt.figure(figsize=(20,4))
+            ax_pred = plt.subplot2grid((4,spp['heat_cols']), (0,spp['pred_start']), colspan=spp['pred_span'])
+            ax_logo = plt.subplot2grid((4,spp['heat_cols']), (1,spp['logo_start']), colspan=spp['logo_span'])
+            ax_sad = plt.subplot2grid((4,spp['heat_cols']), (2,spp['sad_start']), colspan=spp['sad_span'])
+            ax_heat = plt.subplot2grid((4,spp['heat_cols']), (3,0), colspan=spp['heat_cols'])
+
+            # plot predictions
+            plot_predictions(ax_pred, sat_preds[si,:,ti], options.satmut_len)
+
+            # plot sequence weblogo
+            plot_weblogo(ax_logo, seqs[si], sat_loss[:,ti], options.min_limit)
+
+            # plot SAD
+            plot_sad(ax_sad, sat_loss[:,ti], sat_gain[:,ti])
+
+            # plot heat map
+            plot_heat(ax_heat, sat_delta[:,:,ti], options.min_limit)
+
+            plt.savefig('%s/seq%d.pdf' % (options.out_dir,si), dpi=1200)
+            plt.close()
+
+
+def loss_gain(sat_delta, sat_preds_si, satmut_len):
+    # compute min and max
+    sat_min = sat_delta.min(axis=0)
+    sat_max = sat_delta.max(axis=0)
+
+    # determine sat mut region
+    sm_start = (sat_preds_si.shape[0] - satmut_len) // 2
+    sm_end = sm_start + satmut_len
+
+    # compute loss and gain matrixes
+    sat_loss = sat_min - sat_preds_si[sm_start:sm_end,:]
+    sat_gain = sat_max - sat_preds_si[sm_start:sm_end,:]
+
+    return sat_loss, sat_gain
+
+
+def delta_matrix(seqs_1hot, si, sat_preds, satmut_len):
+    ''' Compute the matrix of prediction deltas
+
+    Args:
+        seqs_1hot (SxLx4 array): One-hot coding of all sequences.
+        si (int): Sequence index.
+        sat_preds: (SMxLxT array): Satmut sequence predictions.
+        satmut_len: Saturated mutagenesis region length.
+
+    Returns:
+        sat_delta (4 x L_sm x T array): Delta matrix for saturated mutagenesis region.
+
+    Todo:
+        -Rather than computing the delta as the change at that nucleotide's prediction,
+            compute it as the maximum change across the sequence. That way, we better
+            pick up on motif-flanking interactions.
+    '''
+    seqs_n = int(sat_preds.shape[0] / (1 + 3*satmut_len))
+    num_targets = sat_preds.shape[2]
+
+    # initialize
+    sat_delta = np.zeros((4,satmut_len,num_targets))
+
+    # jump to si's mutated sequences
+    smi = seqs_n + si*3*satmut_len
+
+    # jump to satmut region in preds
+    spi = int((sat_preds.shape[1] - satmut_len) // 2)
+
+    # jump to satmut region in sequence
+    ssi = int((seqs_1hot.shape[1] - satmut_len) // 2)
+
+    # compute delta matrix
+    for li in range(satmut_len):
+        for ni in range(4):
+            if seqs_1hot[si,ssi+li,ni] == 1:
+                # sat_delta[ni,li,:] = sat_preds[si,sli+li,:]
+                sat_delta[ni,li,:] = 0
+            else:
+                # sat_delta[ni,li,:] = sat_preds[si,sli+li,:] - sat_preds[smi,sli+li,:]
+                sat_delta[ni,li,:] = sat_preds[smi,spi+li,:] - sat_preds[si,spi+li,:]
+                smi += 1
+
+    return sat_delta
+
+
+def parse_input(input_file, sample):
+    ''' Parse an input file that might be FASTA or HDF5. '''
+
+    try:
+        # input_file is FASTA
+
+        # load sequences and headers
+        seqs = []
+        seq_headers = []
+        for line in open(input_file):
+            if line[0] == '>':
+                seq_headers.append(line[1:].rstrip())
+                seqs.append('')
+            else:
+                seqs[-1] += line.rstrip()
+
+        # convert to arrays
+        seqs = np.array(seqs)
+        seq_headers = np.array(seq_headers)
+
+        model_input_hdf5 = '%s/model_in.h5'%options.out_dir
+
+        # load sequences
+        seqs_1hot = dna_io.load_sequences(input_file, permute=False)
+        target_labels = False
+        if options.targets_file:
+            target_labels = [line.split()[1] for line in open(options.targets_file)]
+
+        # sample
+        if sample:
+            sample_i = np.array(random.sample(xrange(seqs_1hot.shape[0]), sample))
+            seqs_1hot = seqs_1hot[sample_i]
+            seq_headers = seq_headers[sample_i]
+            seqs = seqs[sample_i]
+
+        # reshape sequences for torch
+        seqs_1hot = seqs_1hot.reshape((seqs_1hot.shape[0],4,1,seqs_1hot.shape[1]/4))
+
+        # write as test data to a HDF5 file
+        h5f = h5py.File(model_input_hdf5, 'w')
+        h5f.create_dataset('test_in', data=seqs_1hot)
+        h5f.close()
+
+    except (UnicodeDecodeError):
+        # input_file is HDF5
+
+        try:
+            model_input_hdf5 = input_file
+
+            # load (sampled) test data from HDF5
+            hdf5_in = h5py.File(input_file, 'r')
+            seqs_1hot = np.array(hdf5_in['test_in'])
+            targets = np.array(hdf5_in['test_out'])
+            # seq_headers = np.array(hdf5_in['test_headers'])
+            target_labels = np.array(hdf5_in['target_labels'])
+            hdf5_in.close()
+
+            # sample
+            if sample:
+                sample_i = np.array(random.sample(range(seqs_1hot.shape[0]), sample))
+                seqs_1hot = seqs_1hot[sample_i]
+                targets = targets[sample_i]
+                # seq_headers = seq_headers[sample_i]
+
+            # convert to ACGT sequences
+            seqs = basenji.io.hot1_dna(seqs_1hot)
+
+        except IOError:
+            parser.error('Could not parse input file as FASTA or HDF5.')
+
+    return seqs, seqs_1hot, targets
+
+
+def plot_heat(ax, sat_delta_ti, min_limit):
+    ''' Plot satmut deltas.
+
+    Args:
+        ax (Axis): matplotlib axis to plot to.
+        sat_delta_ti (4 x L_sm array): Single target delta matrix for saturated mutagenesis region,
+        min_limit (float): Minimum heatmap limit.
+    '''
+    vlim = max(min_limit, abs(sat_delta_ti).max())
+    sns.heatmap(sat_delta_ti, linewidths=0, cmap='RdBu_r', vmin=-vlim, vmax=vlim, xticklabels=False, ax=ax)
+    ax.yaxis.set_ticklabels('TGCA', rotation='horizontal') # , size=10)
+
+
+def plot_predictions(ax, preds, satmut_len):
+    ''' Plot the raw predictions for a sequence and target
+        across the specificed saturated mutagenesis region.
+
+    Args:
+        ax (Axis): matplotlib axis to plot to.
+        preds (L array): Target predictions for one sequence.
+        satmut_len (int): Satmut length from which to determine
+                           the values to plot.
+    '''
+    satmut_start = (preds.shape[0] - satmut_len) // 2
+    satmut_end = satmut_start + satmut_len
+
+    ax.plot(preds[satmut_start:satmut_end], linewidth=1)
+    ax.axhline(0, c='black', linewidth=1, linestyle='--')
+    for axis in ['top','bottom','left','right']:
+        ax.spines[axis].set_linewidth(0.5)
+
+
+def plot_sad(ax, sat_loss_ti, sat_gain_ti):
+    ''' Plot loss and gain SAD scores.
+
+    Args:
+        ax (Axis): matplotlib axis to plot to.
+        sat_loss_ti (L_sm array): Minimum mutation delta across satmut length.
+        sat_gain_ti (L_sm array): Maximum mutation delta across satmut length.
+    '''
+
+    rdbu = sns.color_palette("RdBu_r", 10)
+
+    ax.plot(-sat_loss_ti, c=rdbu[0], label='loss', linewidth=1)
+    ax.plot(sat_gain_ti, c=rdbu[-1], label='gain', linewidth=1)
+    ax.set_xlim(0, len(sat_loss_ti))
+    ax.legend()
+    # ax_sad.grid(True, linestyle=':')
+    for axis in ['top','bottom','left','right']:
+        ax.spines[axis].set_linewidth(0.5)
+
+
+def plot_weblogo(ax, seq, sat_loss_ti, min_limit):
+    ''' Plot height-weighted weblogo sequence.
+
+    Args:
+        ax (Axis): matplotlib axis to plot to.
+        seq ([ACGT]): DNA sequence
+        sat_loss_ti (L_sm array): Minimum mutation delta across satmut length.
+        min_limit (float): Minimum heatmap limit.
+    '''
+    # trim sequence to the satmut region
+    satmut_len = len(sat_loss_ti)
+    satmut_start = int((len(seq) - satmut_len) // 2)
+    satmut_seq = seq[satmut_start:satmut_start+satmut_len]
+
+    # determine nt hieights
+    vlim = max(min_limit, np.max(-sat_loss_ti))
+    seq_heights = 0.25 + 1.75/vlim*(-sat_loss_ti)
+
+    # make logo as eps
+    eps_fd, eps_file = tempfile.mkstemp()
+    seq_logo(satmut_seq, seq_heights, eps_file, color_mode='meme')
+
+    # convert to png
+    png_fd, png_file = tempfile.mkstemp()
+    subprocess.call('convert -density 1200 %s %s' % (eps_file, png_file), shell=True)
+
+    # plot
+    logo = Image.open(png_file)
+    ax.imshow(logo)
+    ax.set_axis_off()
+
+    # clean up
+    os.close(eps_fd)
+    os.remove(eps_file)
+    os.close(png_fd)
+    os.remove(png_file)
+
+
+def satmut_seqs(seqs_1hot, satmut_len):
+    ''' Construct a new array with the given sequences and saturated
+        mutagenesis versions of them. '''
+    seqs_n = seqs_1hot.shape[0]
+    seq_len = seqs_1hot.shape[1]
+    satmut_n = seqs_n + seqs_n*satmut_len*3
+
+    # initialize satmut seqs 1hot
+    sat_seqs_1hot = np.zeros((satmut_n, seq_len, 4))
+
+    # copy over seqs_1hot
+    sat_seqs_1hot[:seqs_n,:,:] = seqs_1hot
+
+    satmut_start = (seq_len - satmut_len) // 2
+    satmut_end = satmut_start + satmut_len
+
+    # add saturated mutagenesis
+    smi = seqs_n
+    for si in range(seqs_n):
+        for li in range(satmut_start, satmut_end):
+            for ni in range(4):
+                if seqs_1hot[si,li,ni] != 1:
+                    # copy sequence
+                    sat_seqs_1hot[smi,:,:] = seqs_1hot[si,:,:]
+
+                    # mutate to ni
+                    sat_seqs_1hot[smi,li,:] = np.zeros(4)
+                    sat_seqs_1hot[smi,li,ni] = 1
+
+                    # update index
+                    smi += 1
+
+    return sat_seqs_1hot
+
+
+def subplot_params(seq_len):
+    ''' Specify subplot layout parameters for various sequence lengths. '''
+    if seq_len < 500:
+        spp = {'heat_cols': 400,
+                'pred_start': 0,
+                'pred_span': 323,
+                'sad_start': 1,
+                'sad_span': 322,
+                'logo_start': 0,
+                'logo_span': 324}
+    else:
+        spp = {'heat_cols': 400,
+                'pred_start': 0,
+                'pred_span': 321,
+                'sad_start': 1,
+                'sad_span': 320,
+                'logo_start': 0,
+                'logo_span': 322}
+
+    return spp
+
+
+################################################################################
+# __main__
+################################################################################
+if __name__ == '__main__':
+    main()

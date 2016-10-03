a3194e1b657c18fcdb9dcd7725aa270e49b1d00e
David Kelley <drk@calicolabs.com>
2016-10-03 11:06:18 -0700

    vcf

--- basenji/__init__.py
+++ basenji/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ['autoencoder', 'batcher', 'dna_io', 'genome', 'gff', 'rnn']
+__all__ = ['autoencoder', 'batcher', 'dna_io', 'genome', 'gff', 'rnn', 'vcf']
 import basenji.autoencoder
 import basenji.batcher
 import basenji.dna_io
 import basenji.genome
 import basenji.gff
 import basenji.rnn
+import basenji.vcf

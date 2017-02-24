f5eea0e64b46eef441eba02bb04580277d886af1
David Kelley <drk@calicolabs.com>
2017-02-23 19:55:22 -0800

    aesthetic improvements

--- basenji/plots.py
+++ basenji/plots.py
@@ -1,50 +1,69 @@
 #!/usr/bin/env python
 
 import matplotlib
-matplotlib.use('Agg')
+matplotlib.use('PDF')
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import spearmanr
 import seaborn as sns
 
-def jointplot(vals1, vals2, out_pdf, alpha=0.5, x_label=None, y_label=None):
+def jointplot(vals1, vals2, out_pdf, alpha=0.5, square=False, x_label=None, y_label=None):
     plt.figure()
 
-    g = sns.jointplot(vals1, vals2, alpha=alpha, color='black', stat_func=spearmanr)
+    g = sns.jointplot(vals1, vals2, color='black', space=0, stat_func=spearmanr, joint_kws={'alpha':alpha, 's':12})
 
     ax = g.ax_joint
-    vmin, vmax = scatter_lims(vals1, vals2)
-    ax.plot([vmin,vmax], [vmin,vmax], linestyle='--', color='black')
 
-    ax.set_xlim(vmin,vmax)
-    ax.set_xlabel(x_label)
-    ax.set_ylim(vmin,vmax)
-    ax.set_ylabel(y_label)
+    if square:
+        vmin, vmax = scatter_lims(vals1, vals2)
+        ax.set_xlim(vmin,vmax)
+        ax.set_ylim(vmin,vmax)
+
+        ax.plot([vmin,vmax], [vmin,vmax], linestyle='--', color='black')
+
+    else:
+        xmin, xmax = scatter_lims(vals1)
+        ax.set_xlim(xmin,xmax)
+        ymin, ymax = scatter_lims(vals2)
+        ax.set_ylim(ymin,ymax)
+
+    if y_label is not None:
+        ax.set_ylabel(y_label)
+    if x_label is not None:
+        ax.set_xlabel(x_label)
 
     ax.grid(True, linestyle=':')
 
-    plt.tight_layout(w_pad=0, h_pad=0)
+    # plt.tight_layout(w_pad=0, h_pad=0)
 
     plt.savefig(out_pdf)
     plt.close()
 
 
-def regplot(vals1, vals2, out_pdf, poly_order=1, alpha=0.5, x_label=None, y_label=None):
+def regplot(vals1, vals2, out_pdf, poly_order=1, alpha=0.5, square=True, x_label=None, y_label=None, title=None):
     plt.figure(figsize=(6,6))
 
-    # g = sns.jointplot(vals1, vals2, alpha=0.5, color='black', stat_func=spearmanr)
     gold = sns.color_palette('husl',8)[1]
     ax = sns.regplot(vals1, vals2, color='black', order=poly_order, scatter_kws={'color':'black', 's':4, 'alpha':alpha}, line_kws={'color':gold})
 
-    vmin, vmax = scatter_lims(vals1, vals2)
-    # ax.plot([vmin,vmax], [vmin,vmax], linestyle='--', color='black')
+    if square:
+        vmin, vmax = scatter_lims(vals1, vals2)
+        ax.set_xlim(vmin,vmax)
+        ax.set_xlim(vmin,vmax)
+    else:
+        xmin, xmax = scatter_lims(vals1)
+        ax.set_xlim(xmin,xmax)
+        ymin, ymax = scatter_lims(vals2)
+        ax.set_xlim(ymin,ymax)
 
-    ax.set_xlim(vmin,vmax)
     if x_label is not None:
         ax.set_xlabel(x_label)
     ax.set_ylim(vmin,vmax)
     if y_label is not None:
         ax.set_ylabel(y_label)
+
+    if title is not None:
+        plt.title(title)
 
     scor, _ = spearmanr(vals1, vals2)
     lim_eps = (vmax-vmin) * .02

52aad7bf1609322792968763c5920923ac2f4004
David Kelley <drk@calicolabs.com>
2017-02-26 18:41:04 -0800

    changes to correlation and stuff

--- basenji/plots.py
+++ basenji/plots.py
@@ -8,7 +8,7 @@ import numpy as np
 from scipy.stats import spearmanr, pearsonr
 import seaborn as sns
 
-def jointplot(vals1, vals2, out_pdf, alpha=0.5, square=False, cor='pearsonr', x_label=None, y_label=None):
+def jointplot(vals1, vals2, out_pdf, alpha=0.5, point_size=10, square=False, cor='pearsonr', x_label=None, y_label=None):
     plt.figure()
 
     if cor.lower() in ['spearman','spearmanr']:
@@ -16,9 +16,9 @@ def jointplot(vals1, vals2, out_pdf, alpha=0.5, square=False, cor='pearsonr', x_
     elif cor.lower() in ['pearson','pearsonr']:
         cor_func = pearsonr
     else:
-        print('Cannot recognize correlation method %s' % cor, file=sys.stderr)
+        cor_func = None
 
-    g = sns.jointplot(vals1, vals2, color='black', space=0, stat_func=cor_func, joint_kws={'alpha':alpha, 's':12})
+    g = sns.jointplot(vals1, vals2, color='black', space=0, stat_func=cor_func, joint_kws={'alpha':alpha, 's':point_size})
 
     ax = g.ax_joint
 
@@ -48,21 +48,20 @@ def jointplot(vals1, vals2, out_pdf, alpha=0.5, square=False, cor='pearsonr', x_
     plt.close()
 
 
-def regplot(vals1, vals2, out_pdf, poly_order=1, alpha=0.5, point_size=4, cor='pearsonr', square=True, x_label=None, y_label=None, title=None):
-    plt.figure(figsize=(6,6))
+def regplot(vals1, vals2, out_pdf, poly_order=1, alpha=0.5, point_size=10, cor='pearsonr', square=True, x_label=None, y_label=None, title=None, figsize=(6,6)):
+    plt.figure(figsize=figsize)
 
     gold = sns.color_palette('husl',8)[1]
     ax = sns.regplot(vals1, vals2, color='black', order=poly_order, scatter_kws={'color':'black', 's':point_size, 'alpha':alpha}, line_kws={'color':gold})
 
     if square:
-        vmin, vmax = scatter_lims(vals1, vals2)
-        ax.set_xlim(vmin,vmax)
-        ax.set_xlim(vmin,vmax)
+        xmin, xmax = scatter_lims(vals1, vals2)
+        ymin, ymax = xmin, xmax
     else:
         xmin, xmax = scatter_lims(vals1)
-        ax.set_xlim(xmin,xmax)
         ymin, ymax = scatter_lims(vals2)
-        ax.set_xlim(ymin,ymax)
+    ax.set_xlim(xmin,xmax)
+    ax.set_ylim(ymin,ymax)
 
     if x_label is not None:
         ax.set_xlabel(x_label)
@@ -72,16 +71,19 @@ def regplot(vals1, vals2, out_pdf, poly_order=1, alpha=0.5, point_size=4, cor='p
     if title is not None:
         plt.title(title)
 
+    corr = None
     if cor.lower() in ['spearman','spearmanr']:
-        scor, _ = spearmanr(vals1, vals2)
-        lim_eps = (vmax-vmin) * .02
-        ax.text(vmin+lim_eps, vmax-3*lim_eps, 'Spearman R: %.3f'%scor, horizontalalignment='left', fontsize=12)
+        corr, _ = spearmanr(vals1, vals2)
+        cor_label = 'Spearman'
     elif cor.lower() in ['pearson','pearsonr']:
-        scor, _ = pearsonr(vals1, vals2)
-        lim_eps = (vmax-vmin) * .02
-        ax.text(vmin+lim_eps, vmax-3*lim_eps, 'Pearson R: %.3f'%scor, horizontalalignment='left', fontsize=12)
-    else:
-        print('Cannot recognize correlation method %s' % cor, file=sys.stderr)
+        corr, _ = pearsonr(vals1, vals2)
+        cor_label = 'Pearson'
+
+    if corr is not None:
+        xlim_eps = (xmax-xmin) * .02
+        ylim_eps = (ymax-ymin) * .02
+
+        ax.text(xmin+xlim_eps, ymax-3*ylim_eps, '%s R: %.3f'%(cor_label,corr), horizontalalignment='left', fontsize=12)
 
     ax.grid(True, linestyle=':')
 

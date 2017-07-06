c21cb17ed4de9c08e2ef20c4749d770416c6c160
David Kelley <drk@calicolabs.com>
2017-07-05 18:54:10 -0700

    install requires

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ with open('LICENSE') as f:
 setup(
     name='basenji',
     version='0.0.1',
-    description='DNA RNNs',
+    description='Sequential regulatory activity machine learning',
     long_description=readme,
     author='David Kelley',
     author_email='drk@calicolabs.com',
-    url='https://github.com/davek44/basenji',
+    url='https://github.com/calico/basenji',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=['h5py','joblib','matplotlib','numpy','pandas','pyBigWig','pysam','scipy','seaborn','sklearn','statsmodels','tensorflow']
 )

5a3d4f3611af03f8344af049e8240e09f6eb3a6f
David Kelley <drk@calicolabs.com>
2017-10-23 09:52:09 -0700

    off by one with Ns

--- basenji/vcf.py
+++ basenji/vcf.py
@@ -203,7 +203,7 @@ def snp_seq1(snp, seq_len, genome_open):
 
   # extract sequence as BED style
   if seq_start < 0:
-    seq = 'N' * (-seq_start) + genome_open.fetch(snp.chrom, 0, seq_end).upper()
+    seq = 'N'*(1-seq_start) + genome_open.fetch(snp.chrom, 0, seq_end).upper()
   else:
     seq = genome_open.fetch(snp.chrom, seq_start - 1, seq_end).upper()
 

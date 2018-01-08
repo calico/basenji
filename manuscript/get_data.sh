e939e077833e1d30a5b586d7c9731410fd72225b
David Kelley <drk@calicolabs.com>
2018-01-08 14:25:43 -0800

    manuscript data and model

---
+++ manuscript/get_data.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://storage.googleapis.com/131k/sample_wigs.txt
+wget https://storage.googleapis.com/131k/l131k_w128.bed
+wget https://storage.googleapis.com/131k/l131k_w128.h5

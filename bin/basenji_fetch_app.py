b20519b48878790c35b443eaa62efe0fe5f763fc
David Kelley <drk@calicolabs.com>
2018-03-01 10:57:34 -0800

    ld via bigquery and memoized sad

--- bin/basenji_fetch_app.py
+++ bin/basenji_fetch_app.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 from optparse import OptionParser
+import collections
+import functools
 import pdb
 
 import numpy as np
+import pandas as pd
 import zarr
+
+from google.cloud import bigquery
 
 import dash
 import dash.dependencies as dd
@@ -23,7 +28,7 @@ Run a Dash app to enable SAD queries.
 def main():
     usage = 'usage: %prog [options] <sad_zarr_file>'
     parser = OptionParser(usage)
-    #parser.add_option()
+    # parser.add_option('--ld', dest='ld_query', default=False, action='store_true')
     (options,args) = parser.parse_args()
 
     if len(args) != 1:
@@ -44,8 +49,9 @@ def main():
     # easy access to target information
     target_ids = sad_zarr_in['target_ids']
     target_labels = sad_zarr_in['target_labels']
-    sad = sad_zarr_in['sad']
 
+    # initialize BigQuery client
+    client = bigquery.Client('seqnn-170614')
 
     #############################################
     # layout
@@ -77,10 +83,14 @@ def main():
                 dcc.Dropdown(
                     id='population',
                     options=[
-                        {'label':'1kG European', 'value':'1kg_euro'},
-                        {'label':'1kG Japanese', 'value':'1kg_jap'}
+                        {'label':'-', 'value':'-'},
+                        {'label':'1kG African', 'value':'1kG_AFR'},
+                        {'label':'1kG American', 'value':'1kG_AMR'},
+                        {'label':'1kG East Asian', 'value':'1kg_EAS'},
+                        {'label':'1kG European', 'value':'1kg_EUR'},
+                        {'label':'1kG South Asian', 'value':'1kg_SAS'}
                     ],
-                    value='1kg_euro'
+                    value='-'
                 )
             ], style={'width': '30%', 'display': 'inline-block'}),
 
@@ -100,7 +110,7 @@ def main():
             dt.DataTable(
                 id='table',
                 rows=[],
-                columns=['SNP', 'Score', 'Experiment', 'Description'],
+                columns=['SNP', 'Association', 'Score', 'R2', 'Experiment', 'Description'],
                 column_widths=[200],
                 editable=False,
                 filterable=True,
@@ -116,31 +126,58 @@ def main():
 
 
     #############################################
+    # callback helpers
+
+    @memoized
+    def read_sad(snp_i):
+        return sad_zarr_in['sad'][snp_i,:].astype('float64')
+
+    def snp_rows(input_snp, ld_r2=1.):
+        rows = []
+
+        # search for SNP
+        if input_snp in snp_indexes:
+            snp_i = snp_indexes[input_snp]
+            snp_sad = np.around(read_sad(snp_i),4)
+            snp_assoc = np.around(snp_sad*ld_r2, 4)
+
+            # extract target scores and info
+            for ti, tid in enumerate(target_ids):
+                rows.append({
+                    'SNP': input_snp,
+                    'Association': snp_assoc[ti],
+                    'Score': snp_sad[ti],
+                    'R2': ld_r2,
+                    'Experiment': tid,
+                    'Description': target_labels[ti]})
+
+        return rows
+
+    #############################################
     # callbacks
 
     @app.callback(
         dd.Output('table', 'rows'),
         [dd.Input('snp_submit', 'n_clicks')],
-        [dd.State('snp_id', 'value')]
+        [dd.State('snp_id', 'value'), dd.State('population', 'value')]
     )
-    def update_table(n_clicks, input_snp):
-        # search for SNP
-        if snp_id not in snp_indexes:
-            raise ValueError('%s not found')
-        else:
-            snp_i = snp_indexes[input_snp]
-            snp_sad = np.around(sad[snp_i,:].astype('float64'),4)
+    def update_table(n_clicks, input_snp, population):
+        # add input_snp rows
+        rows = snp_rows(input_snp)
 
-            # extract target scores and info
-            rows = []
-            for ti, tid in enumerate(target_ids):
-                rows.append({
-                    'SNP': input_snp,
-                    'Score': snp_sad[ti],
-                    'Experiment': tid,
-                    'Description': target_labels[ti]})
+        if population != '-':
+            # construct LD query
+            query = 'SELECT tname, corr'
+            query += ' FROM `genomics-public-data.linkage_disequilibrium_1000G_phase_3.super_pop_EAS`'
+            query += ' WHERE qname = "%s"' % input_snp
 
-            return rows
+            # query to DataFrame
+            query_results = client.query(query)
+
+            for ld_snp, ld_corr in query_results:
+                rows += snp_rows(ld_snp, ld_corr)
+
+        return rows
 
 
     #############################################
@@ -149,6 +186,32 @@ def main():
     app.scripts.config.serve_locally = True
     app.run_server(debug=True)
 
+
+class memoized(object):
+    '''Decorator. Caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned
+    (not reevaluated).
+    '''
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    def __call__(self, *args):
+        if not isinstance(args, collections.Hashable):
+            # uncacheable. a list, for instance.
+            # better to not cache than blow up.
+            return self.func(*args)
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+    def __repr__(self):
+        '''Return the function's docstring.'''
+        return self.func.__doc__
+    def __get__(self, obj, objtype):
+        '''Support instance methods.'''
+        return functools.partial(self.__call__, obj)
 
 ################################################################################
 # __main__

8e37bee0e32dbcc57041761cd6526cc037a063de
David Kelley <drk@calicolabs.com>
2018-06-28 10:03:21 -0700

    preparing for hic training

--- basenji/seqnn.py
+++ basenji/seqnn.py
@@ -227,11 +227,26 @@ class SeqNN(seqnn_util.SeqNNModel):
         ' by the CNN pooling %d') % (self.hp.batch_buffer, pool_preds)
     batch_buffer_pool = self.hp.batch_buffer // pool_preds
 
+    ###################################################
+    # Hi-C layers
+    ###################################################
+
+    # TODO: implement transformation to 2D
+    # TODO: modify the code below to handle 2D
+
+    ###################################################
+    # slice out side buffer
+    ###################################################
+
     # slice out buffer
     seq_length = seqs_repr.shape[1]
     seqs_repr = seqs_repr[:, batch_buffer_pool:
                           seq_length - batch_buffer_pool, :]
-    seq_length = seqs_repr.shape[1]
+    seq_length = seqs_repr.shape[1].value
+    self.preds_length = seq_length
+
+    # save penultimate representation
+    self.penultimate_op = seqs_repr
 
     ###################################################
     # final layer
@@ -383,14 +398,19 @@ class SeqNN(seqnn_util.SeqNNModel):
     # slice buffer
     tstart = self.hp.batch_buffer // self.hp.target_pool
     tend = (self.hp.seq_length - self.hp.batch_buffer) // self.hp.target_pool
-    targets = tf.identity(targets[:, tstart:tend, :], name='targets_op')
+
+    if self.hp.hic:
+      targets = targets[:, :, tstart:tend, :]
+      targets = tf.identity(targets[:, tstart:tend, :, :], name='targets_op')
+    else:
+      targets = tf.identity(targets[:, tstart:tend, :], name='targets_op')
 
     if target_subset is not None:
-      targets = tf.gather(targets, target_subset, axis=2)
+        targets = tf.gather(targets, target_subset, axis=-1)
 
     # clip
     if self.hp.target_clip is not None:
-      targets = tf.clip_by_value(targets, 0, self.hp.target_clip)
+      targets = tf.clip_by_value(targets, -self.hp.target_clip, self.hp.target_clip)
 
     # sqrt
     if self.hp.target_sqrt:
@@ -414,7 +434,12 @@ class SeqNN(seqnn_util.SeqNNModel):
       raise ValueError('Cannot identify loss function %s' % self.hp.loss)
 
     # reduce lossses by batch and position
-    loss_op = tf.reduce_mean(loss_op, axis=[0, 1], name='target_loss')
+    if self.hp.hic:
+      reduce_axes = [0, 1, 2]
+    else:
+      reduce_axes = [0, 1]
+
+    loss_op = tf.reduce_mean(loss_op, axis=reduce_axes, name='target_loss')
     loss_op = tf.check_numerics(loss_op, 'Invalid loss', name='loss_check')
     target_losses = loss_op
 

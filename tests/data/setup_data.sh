ceeb2e0b6f07621f696c550522bb7ff7c26187d5
David Kelley <drk@calicolabs.com>
2018-07-21 10:22:23 -0700

    sad consistency test

---
+++ tests/data/setup_data.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp /home/drk/fetch/data/hgmd_telenti/hg19/splice20_sentinel.vcf hgmd.vcf
+cp /home/drk/common/data/genomes/hg19/regulatory_disease/snps.vcf regulatory_validated.vcf
---
+++ tests/test_sad.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+from optparse import OptionParser
+
+import os
+import pdb
+import subprocess
+import unittest
+
+import h5py
+
+import numpy as np
+
+class TestSAD(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    cls.params_file = 'data/params.txt'
+    cls.model_file = 'data/model_best.tf'
+    cls.vcf_file = 'data/regulatory_validated.vcf'
+    cls.seq_length = 131072
+
+  def test_sad(self):
+    cmd = 'basenji_sad.py --h5 -l %d -o sad/test --rc --shifts "0,1" %s %s %s' % \
+        (self.seq_length, self.params_file, self.model_file, self.vcf_file)
+    subprocess.call(cmd, shell=True)
+
+    saved_h5 = h5py.File('sad/saved/sad.h5', 'r')
+    saved_sad = saved_h5['SAD'][:]
+
+    this_h5 = h5py.File('sad/test/sad.h5', 'r')
+    this_sad = this_h5['SAD'][:]
+
+    np.testing.assert_allclose(saved_sad, this_sad)
+
+  def test_usad(self):
+    cmd = 'basenji_sad.py --h5 -l %d -o sad/utest --rc --shifts "0,1" -u %s %s %s' % \
+        (self.seq_length, self.params_file, self.model_file, self.vcf_file)
+    subprocess.call(cmd, shell=True)
+
+    saved_h5 = h5py.File('sad/usaved/sad.h5', 'r')
+    saved_sad = saved_h5['SAD'][:]
+
+    this_h5 = h5py.File('sad/utest/sad.h5', 'r')
+    this_sad = this_h5['SAD'][:]
+
+    np.testing.assert_allclose(saved_sad, this_sad)
+
+
+################################################################################
+# __main__
+################################################################################
+if __name__ == '__main__':
+  unittest.main()

5b2c71635c3dc81e887c1ba4f7a38a3786a57089
David Kelley <drk@calicolabs.com>
2018-08-29 15:30:39 -0700

    comment

--- bin/basenji_data_write.py
+++ bin/basenji_data_write.py
@@ -161,9 +161,6 @@ def main():
     fasta_open.close()
 
 
-# def _float_feature(value):
-#   return tf.train.Feature(float_list=tf.train.FloatList(value=value))
-
 def _bytes_feature(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 

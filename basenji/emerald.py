632be521cf37b8ab02ce86958da49545f6479d1b
David Kelley <drk@calicolabs.com>
2019-03-15 17:17:45 -0700

    normalization and fetching upgrades

--- basenji/emerald.py
+++ basenji/emerald.py
@@ -16,10 +16,12 @@
 from __future__ import print_function
 
 import pdb
+import os
 import subprocess
 
 import numpy as np
 import pandas as pd
+from pysam import VariantFile
 
 '''
 emerald.py
@@ -31,47 +33,66 @@ class EmeraldVCF:
   def __init__(self, pop_vcf_stem):
     self.pop_vcf_stem = pop_vcf_stem
 
-  def query_ld(self, snp_id, chrm, pos, ld_t=0.1, max_ld_distance=1000000):
+  def fetch(self, chrm, pos_start, pos_end, return_samples=False):
+    vcf_file = '%s.%s.vcf.gz' % (self.pop_vcf_stem, chrm)
+    vcf_open = VariantFile(vcf_file, drop_samples=(not return_samples))
+    return vcf_open.fetch(chrm, pos_start, pos_end)
+
+
+  def query_ld(self, snp_id, chrm, pos,
+               ld_t=0.1, return_pos=False,
+               max_ld_distance=1000000):
     """Retrieve SNPs in LD with the given SNP."""
 
-    # determine search region
-    ld_region_start = max(0, pos - max_ld_distance)
-    ld_region_end = pos + max_ld_distance
-    region_str = '--region %s:%d-%d' % (chrm, ld_region_start, ld_region_end)
+    chr_vcf_file = '%s.%s.vcf.gz' % (self.pop_vcf_stem, chrm)
+    if not os.path.isfile(chr_vcf_file):
+        print('WARNING: %s VCF not found.' % chrm)
+        ld_df = pd.DataFrame()
 
-    # construct emerald command
-    cmd  = 'emeraLD'
-    cmd += ' -i %s.%s.vcf.gz' % (self.pop_vcf_stem, chrm)
-    cmd += ' --rsid %s' % snp_id
-    cmd += ' %s' % region_str
-    cmd += ' --threshold %f' % ld_t
-    cmd += ' --no-phase'
-    cmd += ' --extra'
-    cmd += ' --stdout'
-    cmd += ' 2> /dev/null'
+    else:
+        # determine search region
+        ld_region_start = max(0, pos - max_ld_distance)
+        ld_region_end = pos + max_ld_distance
+        region_str = '--region %s:%d-%d' % (chrm, ld_region_start, ld_region_end)
 
-    ld_snps = [snp_id]
-    ld_r = [1.0]
-    ld_pos = [pos]
+        # construct emerald command
+        cmd  = 'emeraLD'
+        cmd += ' -i %s' % chr_vcf_file
+        cmd += ' --rsid %s' % snp_id
+        cmd += ' %s' % region_str
+        cmd += ' --threshold %f' % ld_t
+        cmd += ' --no-phase'
+        cmd += ' --extra'
+        cmd += ' --stdout'
+        cmd += ' 2> /dev/null'
 
-    # parse returned lines
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-    for line in proc.stdout:
-        line = line.decode('UTF-8')
-        if not line.startswith('#'):
-            a = line.split()
-            ld_pos.append(int(a[4]))
-            ld_snps.append(a[5])
-            ld_r.append(float(a[7]))
-    proc.communicate()
+        ld_snps = [snp_id]
+        ld_r = [1.0]
+        ld_pos = [pos]
 
-    # sort by position
-    sort_indexes = np.argsort(ld_pos)
-    ld_snps = np.array(ld_snps)[sort_indexes]
-    ld_r = np.array(ld_r)[sort_indexes]
+        # parse returned lines
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+        for line in proc.stdout:
+            line = line.decode('UTF-8')
+            if not line.startswith('#'):
+                a = line.split()
+                ld_pos.append(int(a[4]))
+                ld_snps.append(a[5])
+                ld_r.append(float(a[7]))
+        proc.communicate()
 
-    # construct data frame
-    ld_df = pd.DataFrame()
-    ld_df['snp'] = ld_snps
-    ld_df['r'] = ld_r
+        # sort by position
+        sort_indexes = np.argsort(ld_pos)
+        ld_snps = np.array(ld_snps)[sort_indexes]
+        ld_r = np.array(ld_r)[sort_indexes]
+
+        # construct data frame
+        ld_df = pd.DataFrame()
+        ld_df['snp'] = ld_snps
+        ld_df['r'] = ld_r
+
+        if return_pos:
+            ld_df['chr'] = chrm
+            ld_df['pos'] = np.array(ld_pos)[sort_indexes]
+
     return ld_df

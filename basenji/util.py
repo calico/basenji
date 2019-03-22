b754684e8a968ed24f00222e6f7bc80fc56c48b5
David Kelley <drk@calicolabs.com>
2019-03-22 12:20:15 -0700

    util exec_par

---
+++ basenji/util.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import operator, os, sys, subprocess, time
+
+
+############################################################
+# exec_par
+#
+# Execute the commands in the list 'cmds' in parallel, but
+# only running 'max_proc' at a time.
+############################################################
+def exec_par(cmds, max_proc=None, verbose=False):
+    total = len(cmds)
+    finished = 0
+    running = 0
+    p = []
+
+    if max_proc == None:
+        max_proc = len(cmds)
+
+    if max_proc == 1:
+        while finished < total:
+            if verbose:
+                print(cmds[finished], file=sys.stderr)
+            op = subprocess.Popen(cmds[finished], shell=True)
+            os.waitpid(op.pid, 0)
+            finished += 1
+
+    else:
+        while finished + running < total:
+            # launch jobs up to max
+            while running < max_proc and finished+running < total:
+                if verbose:
+                    print(cmds[finished+running], file=sys.stderr)
+                p.append(subprocess.Popen(cmds[finished+running], shell=True))
+                #print 'Running %d' % p[running].pid
+                running += 1
+
+            # are any jobs finished
+            new_p = []
+            for i in range(len(p)):
+                if p[i].poll() != None:
+                    running -= 1
+                    finished += 1
+                else:
+                    new_p.append(p[i])
+
+            # if none finished, sleep
+            if len(new_p) == len(p):
+                time.sleep(1)
+            p = new_p
+
+        # wait for all to finish
+        for i in range(len(p)):
+            p[i].wait()

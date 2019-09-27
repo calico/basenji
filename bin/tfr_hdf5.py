28f6dbec4bee2572fa7f94445d63cebb2de6dc9b
David Kelley <drk@calicolabs.com>
2019-09-27 11:33:14 -0700

    update tf2

--- bin/tfr_hdf5.py
+++ bin/tfr_hdf5.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 from optparse import OptionParser
+import glob
 import os
 
 import h5py
+from natsort import natsorted
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-
-from basenji import tfrecord_batcher
+if tf.__version__[0] == '1':
+  tf.compat.v1.enable_eager_execution()
 
 '''
 tfr_hdf5.py
@@ -19,35 +21,23 @@ Convert HDF5 training file to TFRecords.
 # main
 ################################################################################
 def main():
-  usage = 'usage: %prog [options] <targets_file> <tfr_dir> <hdf5_file>'
+  usage = 'usage: %prog [options] <data_dir> <hdf5_file>'
   parser = OptionParser(usage)
-  parser.add_option('-p', dest='processes', default=16, type='int',
-      help='Number of parallel threads to use [Default: %default]')
-  parser.add_option('-s', dest='shards', default=16, type='int',
-      help='Number of sharded files to output per dataset [Default: %default]')
-  parser.add_option('-l', dest='seq_len',
-      type='int', default=131072,
-      help='Sequence length [Default: %default]')
-  parser.add_option('-w', dest='pool_width',
-      default=128, type='int',
-      help='Pool width [Default: %default]')
   (options,args) = parser.parse_args()
 
-  if len(args) != 3:
-    parser.error('Must provide targets file, TFRecords data directory, and output HDF5 file')
+  if len(args) != 2:
+    parser.error('Must provide data directory and output HDF5 file')
   else:
-    targets_file = args[0]
-    tfr_data_dir = args[1]
-    hdf5_file = args[2]
+    data_dir = args[0]
+    hdf5_file = args[1]
 
   # read target datasets
-  targets_df = pd.read_table(targets_file, index_col=0)
+  targets_file = '%s/targets.txt' % data_dir
+  targets_df = pd.read_csv(targets_file, index_col=0, sep='\t')
+  num_targets = targets_df.shape[0]
 
   # write to HDF5
   hdf5_out = h5py.File(hdf5_file, 'w')
-
-  # store pooling
-  hdf5_out.create_dataset('pool_width', data=options.pool_width, dtype='int')
 
   # store targets
   target_ids = np.array(targets_df.identifier, dtype='S')
@@ -62,13 +52,11 @@ def main():
     target_strands = np.array(['*']*targets_df.shape[0], dtype='S')
   hdf5_out.create_dataset('target_strands', data=target_strands)
 
-  target_len = options.seq_len // options.pool_width
-
   for data_set in ['train', 'valid', 'test']:
-    tfr_pattern = '%s/tfrecords/%s-*.tfr' % (tfr_data_dir, data_set)
+    tfr_pattern = '%s/tfrecords/%s-*.tfr' % (data_dir, data_set)
     print(tfr_pattern)
 
-    seqs_1hot, targets = read_tfr(tfr_pattern, target_len)
+    seqs_1hot, targets = read_tfr(tfr_pattern, num_targets)
     print(' seqs_1hot', seqs_1hot.shape)
     print(' targets', targets.shape)
 
@@ -82,47 +70,36 @@ def main():
         data=targets,
         dtype='float16')
 
-    hdf5_out.create_dataset(
-        '%s_na' % data_set,
-        data=np.zeros((targets.shape[0], targets.shape[1]), dtype='bool'),
-        dtype='bool')
-
   hdf5_out.close()
 
 
-def read_tfr(tfr_pattern, target_len):
-  tfr_files = tfrecord_batcher.order_tfrecords(tfr_pattern)
+def read_tfr(tfr_pattern, num_targets):
+  tfr_files = natsorted(glob.glob(tfr_pattern))
   if tfr_files:
     dataset = tf.data.Dataset.list_files(tf.constant(tfr_files), shuffle=False)
   else:
     dataset = tf.data.Dataset.list_files(tfr_pattern)
   dataset = dataset.flat_map(file_to_records)
-  dataset = dataset.batch(1)
   dataset = dataset.map(parse_proto)
-
-  iterator = dataset.make_one_shot_iterator()
-
-  next_op = iterator.get_next()
+  dataset = dataset.batch(1)
 
   seqs_1hot = []
   targets = []
 
-  with tf.Session() as sess:
-    next_datum = sess.run(next_op)
-    while next_datum:
-      seq_1hot = next_datum['sequence'].reshape((-1,4))
-      targets1 = next_datum['targets'].reshape(target_len,-1)
-
+  si = 0
+  for seq_1hot, targets1 in dataset:
+    # TEMP!
+    if si % 2 == 0:
+      seq_1hot = seq_1hot.numpy()[0].astype('uint8')
+      targets1 = targets1.numpy()[0].astype('float16')
+      seq_1hot = seq_1hot.reshape((-1,4))
+      targets1 = targets1.reshape((-1,num_targets))
       seqs_1hot.append(seq_1hot)
       targets.append(targets1)
+    si += 1
 
-      try:
-        next_datum = sess.run(next_op)
-      except tf.errors.OutOfRangeError:
-        next_datum = False
-
-  seqs_1hot = np.array(seqs_1hot)
-  targets = np.array(targets)
+  seqs_1hot = np.array(seqs_1hot, dtype='uint8')
+  targets = np.array(targets, dtype='float16')
 
   return seqs_1hot, targets
 
@@ -132,13 +109,13 @@ def file_to_records(filename):
 
 def parse_proto(example_protos):
   features = {
-    'sequence': tf.FixedLenFeature([], tf.string),
-    'target': tf.FixedLenFeature([], tf.string)
+    'sequence': tf.io.FixedLenFeature([], tf.string),
+    'target': tf.io.FixedLenFeature([], tf.string)
   }
-  parsed_features = tf.parse_example(example_protos, features=features)
-  seq = tf.decode_raw(parsed_features['sequence'], tf.uint8)
-  targets = tf.decode_raw(parsed_features['target'], tf.float16)
-  return {'sequence': seq, 'targets': targets}
+  parsed_features = tf.io.parse_single_example(example_protos, features=features)
+  seq = tf.io.decode_raw(parsed_features['sequence'], tf.uint8)
+  targets = tf.io.decode_raw(parsed_features['target'], tf.float16)
+  return seq, targets
 
 
 ################################################################################

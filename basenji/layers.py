4454c0278ea00bb5dc14feba9da3ed856249441d
David Kelley <drk@calicolabs.com>
2019-10-08 17:35:29 -0700

    copyright

--- basenji/layers.py
+++ basenji/layers.py
@@ -1,11 +1,22 @@
-"""Wrapper code for using commonly-used layers."""
+# Copyright 2019 Calico LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =========================================================================
 import sys
 import tensorflow as tf
 
 from tensor2tensor.layers.common_attention import attention_bias_proximal
 from tensor2tensor.layers.common_attention import _generate_relative_positions_embeddings, _relative_attention_inner
-
-from basenji import ops
 
 ############################################################
 # Keras Layers

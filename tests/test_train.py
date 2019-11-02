5988ca903f16d8eb3eb04bbd94ba6640e11ed986
David Kelley <drk@calicolabs.com>
2019-11-02 15:12:17 -0700

    test checkpoint

--- tests/test_train.py
+++ tests/test_train.py
@@ -39,7 +39,7 @@ class TestTrain(unittest.TestCase):
       # basenji train
       basenji_cmd = '. /home/drk/anaconda3/etc/profile.d/conda.sh;'
       basenji_cmd += ' conda activate %s;' % self.conda_env
-      basenji_cmd += ' /home/drk/code/basenji2/bin/basenji_train.py'
+      basenji_cmd += ' basenji_train.py'
       basenji_cmd += ' -o %s/train' % it_dir
       basenji_cmd += ' %s' % self.params_file
       basenji_cmd += ' %s' % self.data_dir
@@ -58,7 +58,7 @@ class TestTrain(unittest.TestCase):
     slurm.multi_run(jobs, verbose=True)
 
     ################################################################
-    # test
+    # test check
     ################################################################
     jobs = []
     for i in range(self.iterations):
@@ -67,7 +67,35 @@ class TestTrain(unittest.TestCase):
       # basenji test
       basenji_cmd = '. /home/drk/anaconda3/etc/profile.d/conda.sh;'
       basenji_cmd += ' conda activate %s;' % self.conda_env
-      basenji_cmd += ' /home/drk/code/basenji2/bin/basenji_test.py'
+      basenji_cmd += ' basenji_test.py'
+      basenji_cmd += ' -o %s/test_train' % it_dir
+      basenji_cmd += ' --tfr "train-*.tfr"'
+      basenji_cmd += ' %s' % self.params_file
+      basenji_cmd += ' %s/train/model_check.h5' % it_dir
+      basenji_cmd += ' %s' % self.data_dir
+
+      basenji_job = slurm.Job(basenji_cmd,
+                      name='test%d' % i,
+                      out_file='%s/test_train.out'%it_dir,
+                      err_file='%s/test_train.err'%it_dir,
+                      queue=self.queue,
+                      cpu=1,
+                      gpu=1,
+                      mem=23000,
+                      time='1:00:00')
+      jobs.append(basenji_job)
+
+    ################################################################
+    # test best
+    ################################################################
+    jobs = []
+    for i in range(self.iterations):
+      it_dir = '%s/%d' % (exp_dir, i)
+
+      # basenji test
+      basenji_cmd = '. /home/drk/anaconda3/etc/profile.d/conda.sh;'
+      basenji_cmd += ' conda activate %s;' % self.conda_env
+      basenji_cmd += ' basenji_test.py'
       basenji_cmd += ' -o %s/test' % it_dir
       basenji_cmd += ' %s' % self.params_file
       basenji_cmd += ' %s/train/model_best.h5' % it_dir
@@ -85,9 +113,31 @@ class TestTrain(unittest.TestCase):
       jobs.append(basenji_job)
 
     slurm.multi_run(jobs, verbose=True)
+    
 
     ################################################################
-    # compare
+    # compare checkpoint on training set
+    ################################################################
+    ref_cors = []
+    for acc_file in glob.glob('%s/*/test_train/acc.txt' % self.ref_dir):
+      acc_df = pd.read_csv(acc_file, sep='\t', index_col=0)
+      ref_cors.append(acc_df.pearsonr.mean())
+
+    exp_cors = []
+    for acc_file in glob.glob('%s/*/test_train/acc.txt' % exp_dir):
+      acc_df = pd.read_csv(acc_file, sep='\t', index_col=0)
+      exp_cors.append(acc_df.pearsonr.mean())
+
+    _, pval = mannwhitneyu(ref_cors, exp_cors, alternative='two-sided')
+    print('\nTrain:')
+    print('Reference  PearsonR: %.4f (%.4f)' % (np.mean(ref_cors), np.std(ref_cors)))
+    print('Experiment PearsonR: %.4f (%.4f)' % (np.mean(exp_cors), np.std(exp_cors)))
+    print('Mann-Whitney U p-value: %.3g' % pval)
+
+    self.assertGreater(pval, 0.05)
+    
+    ################################################################
+    # compare best on test set
     ################################################################
     ref_cors = []
     for acc_file in glob.glob('%s/*/test/acc.txt' % self.ref_dir):
@@ -100,6 +150,7 @@ class TestTrain(unittest.TestCase):
       exp_cors.append(acc_df.pearsonr.mean())
 
     _, pval = mannwhitneyu(ref_cors, exp_cors, alternative='two-sided')
+    print('\nTest:')
     print('Reference  PearsonR: %.4f (%.4f)' % (np.mean(ref_cors), np.std(ref_cors)))
     print('Experiment PearsonR: %.4f (%.4f)' % (np.mean(exp_cors), np.std(exp_cors)))
     print('Mann-Whitney U p-value: %.3g' % pval)

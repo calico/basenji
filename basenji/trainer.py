6c5c819c51db960a3e73ba827592b32b783dff8f
David Kelley <drk@calicolabs.com>
2019-11-05 08:39:32 -0800

    direct losses

--- basenji/trainer.py
+++ basenji/trainer.py
@@ -14,6 +14,7 @@
 # =========================================================================
 """SeqNN trainer"""
 import time
+import pdb
 
 import numpy as np
 import tensorflow as tf
@@ -48,7 +49,6 @@ class Trainer:
     self.train_epochs = self.params.get('train_epochs', 1000)
 
   def compile(self, model):
-
     num_targets = model.output_shape[-1]
     model.compile(loss=self.loss_fn,
                   optimizer=self.optimizer,
@@ -80,16 +80,16 @@ class Trainer:
 
     # metrics
     num_targets = model.output_shape[-1]
-    train_loss = tf.keras.metrics.Poisson()
     train_r = metrics.PearsonR(num_targets)
     valid_r = metrics.PearsonR(num_targets)
+    train_loss = tf.keras.metrics.Mean()
 
-    # @tf.function
+    @tf.function
     def train_step(x, y):
       with tf.GradientTape() as tape:
-        pred = model(x, training=True)
-        loss = self.loss_fn(y, pred)
-      train_loss(y, pred)
+        pred = model(x, training=tf.constant(True))
+        loss = self.loss_fn(y, pred) + sum(model.losses)
+      train_loss(loss)
       train_r(y, pred)
       gradients = tape.gradient(loss, model.trainable_variables)
       self.optimizer.apply_gradients(zip(gradients, model.trainable_variables))

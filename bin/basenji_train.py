922ccd3e920740d533092e7bfca3a013bfafb364
David Kelley <drk@calicolabs.com>
2020-04-20 11:23:54 -0700

    invisible gpu

--- bin/basenji_train.py
+++ bin/basenji_train.py
@@ -24,9 +24,6 @@ import time
 
 import numpy as np
 
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = '-1'
-
 import tensorflow as tf
 if tf.__version__[0] == '1':
   tf.compat.v1.enable_eager_execution()

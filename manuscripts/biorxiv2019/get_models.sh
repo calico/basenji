872b0d492a08e1ee275b0c4187d4cb1850f91896
David Kelley <drk@calicolabs.com>
2020-05-29 15:04:58 -0700

    .tf models

--- manuscripts/biorxiv2019/get_models.sh
+++ manuscripts/biorxiv2019/get_models.sh
@@ -2,3 +2,5 @@
 
 wget https://storage.googleapis.com/basenji_barnyard/model_human.h5
 wget https://storage.googleapis.com/basenji_barnyard/model_mouse.h5
+
+# also have .tf but they are directories, so easier to use gsutil cp -r

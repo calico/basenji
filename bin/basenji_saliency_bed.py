c81ac1eed809e7a2a7ff9cc20a68af10c2db87b0
DivyanshiSrivastava <divyanshi@calicolabs.com>
2022-03-14 11:19:31 -0700

    Remove multiprocessing options

--- bin/basenji_saliency_bed.py
+++ bin/basenji_saliency_bed.py
@@ -49,9 +49,6 @@ def main():
     parser.add_option('--plots', dest='plots',
                       default=False, action='store_true',
                       help='Make heatmap plots [Default: %default]')
-    parser.add_option('-p', dest='processes',
-                      default=None, type='int',
-                      help='Number of processes, passed by multi script')
     parser.add_option('--rc', dest='rc',
                       default=False, action='store_true',
                       help='Ensemble forward and reverse complement predictions [Default: %default]')
@@ -70,39 +67,9 @@ def main():
     (options, args) = parser.parse_args()
 
     if len(args) == 3:
-        # single worker
         params_file = args[0]
         model_file = args[1]
         bed_file = args[2]
-
-    elif len(args) == 4:
-        # master script
-        options_pkl_file = args[0]
-        params_file = args[1]
-        model_file = args[2]
-        bed_file = args[3]
-
-        # load options
-        options_pkl = open(options_pkl_file, 'rb')
-        options = pickle.load(options_pkl)
-        options_pkl.close()
-
-    elif len(args) == 5:
-        # multi worker
-        options_pkl_file = args[0]
-        params_file = args[1]
-        model_file = args[2]
-        bed_file = args[3]
-        worker_index = int(args[4])
-
-        # load options
-        options_pkl = open(options_pkl_file, 'rb')
-        options = pickle.load(options_pkl)
-        options_pkl.close()
-
-        # update output directory
-        options.out_dir = '%s/job%d' % (options.out_dir, worker_index)
-
     else:
         parser.error('Must provide parameter and model files and BED file')
 
@@ -151,12 +118,6 @@ def main():
     # read sequences from BED
     seqs_dna, seqs_coords = bed.make_bed_seqs(
         bed_file, options.genome_fasta, params_model['seq_length'], stranded=True)
-
-    # filter for worker SNPs
-    if options.processes is not None:
-        worker_bounds = np.linspace(0, len(seqs_dna), options.processes + 1, dtype='int')
-        seqs_dna = seqs_dna[worker_bounds[worker_index]:worker_bounds[worker_index + 1]]
-        seqs_coords = seqs_coords[worker_bounds[worker_index]:worker_bounds[worker_index + 1]]
 
     num_seqs = len(seqs_dna)
 

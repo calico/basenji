9ec62c837fa32b0dca89a7559d947cacadfa07b9
David Kelley <drk@calicolabs.com>
2022-05-23 10:56:33 -0700

    test genes

--- bin/borzoi_test_genes.py
+++ bin/borzoi_test_genes.py
@@ -16,16 +16,19 @@
 
 from optparse import OptionParser
 import json
+import pdb
 import os
 import time
 
 import h5py
+from intervaltree import IntervalTree
 import numpy as np
 import pandas as pd
 import pybedtools
 from scipy.stats import pearsonr
 from sklearn.metrics import explained_variance_score
 import tensorflow as tf
+from tqdm import tqdm
 
 from basenji import bed
 from basenji import dataset
@@ -35,9 +38,9 @@ import pygene
 from quantile_normalization import quantile_normalize
 
 '''
-basenji_test_rna.py
+borzoi_test_genes.py
 
-Additional test metrics for RNA-seq tracks.
+Measure accuracy at gene-level.
 '''
 
 ################################################################################
@@ -50,7 +53,7 @@ def main():
       default=0, type='int',
       help='Parameters head [Default: %default]')
   parser.add_option('-o', dest='out_dir',
-      default='rna_out',
+      default='testg_out',
       help='Output directory for predictions [Default: %default]')
   parser.add_option('--rc', dest='rc',
       default=False, action='store_true',
@@ -58,6 +61,9 @@ def main():
   parser.add_option('--shifts', dest='shifts',
       default='0',
       help='Ensemble prediction shifts [Default: %default]')
+  parser.add_option('--span', dest='span',
+      default=False, action='store_true',
+      help='Aggregate entire gene span [Default: %default]')
   parser.add_option('-t', dest='targets_file',
       default=None, type='str',
       help='File specifying target indexes and labels in table format')
@@ -91,13 +97,22 @@ def main():
     options.targets_file = '%s/targets.txt' % data_dir
   targets_df = pd.read_csv(options.targets_file, index_col=0, sep='\t')
 
-  # slice RNA
-  rna_indexes = []
-  for ti, desc in enumerate(targets_df.description):
-    if desc.startswith('RNA:') or desc.startswith('scRNA:'):
-      rna_indexes.append(ti)
-  targets_rna_df = targets_df.iloc[rna_indexes]
-  num_targets = targets_rna_df.shape[0]
+  # attach strand
+  targets_strand = []
+  for ti, identifier in enumerate(targets_df.identifier):
+    if targets_df.strand_pair.iloc[ti] == ti:
+      targets_strand.append('.')
+    else:
+      targets_strand.append(identifier[-1])
+  targets_df['strand'] = targets_strand
+
+  # collapse stranded
+  strand_mask = (targets_df.strand != '-')
+  targets_strand_df = targets_df[strand_mask]
+
+  # count targets
+  num_targets = targets_df.shape[0]
+  num_targets_strand = targets_strand_df.shape[0]
 
   # read model parameters
   with open(params_file) as params_open:
@@ -115,18 +130,8 @@ def main():
   # initialize model
   seqnn_model = seqnn.SeqNN(params_model)
   seqnn_model.restore(model_file, options.head_i)
+  seqnn_model.build_slice(targets_df.index)
   seqnn_model.build_ensemble(options.rc, options.shifts)
-  seqnn_model.build_slice(rna_indexes)
-
-  #######################################################
-  # predict
-  
-  # compute predictions
-  test_preds_rna = seqnn_model.predict(eval_data).astype('float16')
-
-  # read targets
-  test_targets = eval_data.numpy(return_inputs=False)
-  test_targets_rna = test_targets[...,rna_indexes]
   
   #######################################################
   # sequence intervals
@@ -143,83 +148,98 @@ def main():
     names=['chr','start','end','split'])
   seqs_df = seqs_df[seqs_df.split == options.split_label]
 
-  # write intervals BED
-  intervals_bed_file = '%s/intervals.bed' % options.out_dir
-  intervals_bed_out = open(intervals_bed_file, 'w')
-  for si, seq in enumerate(seqs_df.itertuples()):
-    bin_start = seq.start + crop_bp
-    for bi in range(target_length):
-      bin_end = bin_start + pool_width
-      cols = [seq.chr, str(bin_start), str(bin_end), '%d/%d' % (si,bi)]
-      print('\t'.join(cols), file=intervals_bed_out)
-      bin_start = bin_end
-  intervals_bed_out.close()
-
   #######################################################
-  # genes
+  # make gene BED
 
-  # read genes
-  genes_gtf = pygene.GTF(genes_gtf_file)
+  genes_bed_file = '%s/genes.bed' % options.out_dir
+  if options.span:
+    make_genes_span(genes_bed_file, genes_gtf_file, options.out_dir)
+  else:
+    make_genes_exon(genes_bed_file, genes_gtf_file, options.out_dir)
 
-  # write gene spans
-  genes_bed_file = '%s/genes_all.bed' % options.out_dir
-  genes_bed_out = open(genes_bed_file, 'w')
+  # count gene normalization lengths
   gene_lengths = {}
-  for gene_id, gene in genes_gtf.genes.items():
-    start, end = gene.span()
-    gene_lengths[gene_id] = end - start
-    cols = [gene.chrom, str(start), str(end), gene_id]
-    print('\t'.join(cols), file=genes_bed_out)
-  genes_bed_out.close()
-
-  # find overlapping genes
-  genes1_bt = pybedtools.BedTool(genes_bed_file)
-  genes2_bt = pybedtools.BedTool(genes_bed_file)
-  overlapping_genes = set()
-  for overlap in genes1_bt.intersect(genes2_bt, wo=True):
-    gene1_id = overlap[3]
-    gene2_id = overlap[7]
-    if gene1_id != gene2_id:
-      overlapping_genes.add(gene1_id)
-      overlapping_genes.add(gene2_id)
-
-  # filter for nonoverlapping genes
-  ugenes_bed_file = '%s/genes_unique.bed' % options.out_dir
-  ugenes_bed_out = open(ugenes_bed_file, 'w')
+  gene_strand = {}
   for line in open(genes_bed_file):
-    gene_id = line.split()[-1]
-    if gene_id not in overlapping_genes:
-      print(line, end='', file=ugenes_bed_out)
-  ugenes_bed_out.close()
+    a = line.rstrip().split('\t')
+    gene_id = a[3]
+    gene_seg_len = int(a[2]) - int(a[1])
+    gene_lengths[gene_id] = gene_lengths.get(gene_id,0) + gene_seg_len
+    gene_strand[gene_id] = a[5]
 
   #######################################################
   # intersect genes w/ preds, targets
 
-  intervals_bt = pybedtools.BedTool(intervals_bed_file)
-  genes_bt = pybedtools.BedTool(ugenes_bed_file)
+  genes_bt = pybedtools.BedTool(genes_bed_file)
 
   # hash preds/targets by gene_id
   gene_preds_dict = {}
   gene_targets_dict = {}
 
-  for overlap in genes_bt.intersect(intervals_bt, wo=True):
-    gene_id = overlap[3]
-    si, bi = overlap[7].split('/')
-    si = int(si)
-    bi = int(bi)
-    gene_preds_dict.setdefault(gene_id,[]).append(test_preds_rna[si,bi])
-    gene_targets_dict.setdefault(gene_id,[]).append(test_targets_rna[si,bi])
+  si = 0
+  for x, y in tqdm(eval_data.dataset):
+    # predict only if gene overlaps
+    yh = None
+
+    # assemble sequence bedtool
+    seq_bed_lines = []
+    for bsi in range(x.shape[0]):
+      seq = seqs_df.iloc[si+bsi]
+      seq_bed_lines.append('%s %d %d %d' % (seq.chr, seq.start, seq.end, bsi))
+    seq_bedt = pybedtools.BedTool('\n'.join(seq_bed_lines), from_string=True)
+
+    for overlap in genes_bt.intersect(seq_bedt, wo=True):
+      gene_id = overlap[3]
+      gene_start = int(overlap[1])
+      gene_end = int(overlap[2])
+      seq_start = int(overlap[7])
+      bsi = int(overlap[9])
+
+      if yh is None:
+        # predict
+        yh = seqnn_model.ensemble.predict(x)
+        
+      # clip boundaries
+      gene_seq_start = max(0, gene_start - seq_start)
+      gene_seq_end = max(0, gene_end - seq_start)
+
+      # requires >50% overlap
+      bin_start = int(np.round(gene_seq_start / pool_width))
+      bin_end = int(np.round(gene_seq_end / pool_width))
+
+      # slice gene region
+      yhb = yh[bsi,bin_start:bin_end]
+      yb = y[bsi,bin_start:bin_end]
+
+      if len(yb) > 0:  
+        gene_preds_dict.setdefault(gene_id,[]).append(yhb)
+        gene_targets_dict.setdefault(gene_id,[]).append(yb)
+
+    # advance sequence table index
+    si += x.shape[0]
 
   # aggregate gene bin values into arrays
   gene_targets = []
   gene_preds = []
+  gene_ids = sorted(gene_targets_dict.keys())
 
-  for gene_id in sorted(gene_targets_dict.keys()):
-    gene_preds_gi = np.array(gene_preds_dict[gene_id])
-    gene_targets_gi = np.array(gene_targets_dict[gene_id])
+  for gene_id in gene_ids:
+    gene_preds_gi = np.concatenate(gene_preds_dict[gene_id], axis=0)
+    gene_targets_gi = np.concatenate(gene_targets_dict[gene_id], axis=0)
 
-    gene_preds_gi = gene_preds_gi.mean(axis=0) * gene_lengths[gene_id]
-    gene_targets_gi = gene_targets_gi.mean(axis=0) * gene_lengths[gene_id]
+    # slice strand
+    if gene_strand[gene_id] == '+':
+      gene_strand_mask = (targets_df.strand != '-')
+    else:
+      gene_strand_mask = (targets_df.strand != '+')
+    gene_preds_gi = gene_preds_gi[:,gene_strand_mask]
+    gene_targets_gi = gene_targets_gi[:,gene_strand_mask]
+
+    if gene_targets_gi.shape[0] == 0:
+      print(gene_id, gene_targets_gi.shape, gene_preds_gi.shape)
+
+    gene_preds_gi = gene_preds_gi.mean(axis=0) # * gene_lengths[gene_id]
+    gene_targets_gi = gene_targets_gi.mean(axis=0) # * gene_lengths[gene_id]
 
     gene_preds.append(gene_preds_gi)
     gene_targets.append(gene_targets_gi)
@@ -233,6 +253,12 @@ def main():
   gene_preds_norm = quantile_normalize(gene_preds)
   gene_preds_norm = gene_preds_norm - gene_preds_norm.mean(axis=-1, keepdims=True)
 
+  # save values
+  genes_targets_df = pd.DataFrame(gene_targets, index=gene_ids)
+  genes_targets_df.to_csv('%s/gene_targets.tsv' % options.out_dir, sep='\t')
+  genes_preds_df = pd.DataFrame(gene_preds, index=gene_ids)
+  genes_preds_df.to_csv('%s/gene_preds.tsv' % options.out_dir, sep='\t') 
+
   #######################################################
   # accuracy stats
 
@@ -240,7 +266,7 @@ def main():
   acc_r2 = []
   acc_npearsonr = []
   acc_nr2 = []
-  for ti in range(num_targets):
+  for ti in range(num_targets_strand):
     r_ti = pearsonr(gene_targets[:,ti], gene_preds[:,ti])[0]
     acc_pearsonr.append(r_ti)
     r2_ti = explained_variance_score(gene_targets[:,ti], gene_preds[:,ti])
@@ -251,12 +277,12 @@ def main():
     acc_nr2.append(nr2_ti)
 
   acc_df = pd.DataFrame({
-    'identifier': targets_rna_df.identifier,
+    'identifier': targets_strand_df.identifier,
     'pearsonr': acc_pearsonr,
     'r2': acc_r2,
     'pearsonr_norm': acc_npearsonr,
     'r2_norm': acc_nr2,
-    'description': targets_rna_df.description
+    'description': targets_strand_df.description
     })
   acc_df.to_csv('%s/acc.txt' % options.out_dir, sep='\t')
 
@@ -279,6 +305,90 @@ def genes_aggregate(genes_bed_file, values_bedgraph):
     gene_values[gene_id] = gene_values.get(gene_id,0) + value
 
   return gene_values
+
+
+def make_genes_span(genes_bed_file, genes_gtf_file, out_dir, stranded=True):
+  # read genes
+  genes_gtf = pygene.GTF(genes_gtf_file)
+
+  # write all gene spans
+  agenes_bed_file = '%s/genes_all.bed' % out_dir
+  agenes_bed_out = open(agenes_bed_file, 'w')
+  for gene_id, gene in genes_gtf.genes.items():
+    start, end = gene.span()
+    cols = [gene.chrom, str(start-1), str(end), gene_id, '.', gene.strand]
+    print('\t'.join(cols), file=agenes_bed_out)
+  agenes_bed_out.close()
+
+  # find overlapping genes
+  genes1_bt = pybedtools.BedTool(agenes_bed_file)
+  genes2_bt = pybedtools.BedTool(agenes_bed_file)
+  overlapping_genes = set()
+  for overlap in genes1_bt.intersect(genes2_bt, s=stranded, wo=True):
+    gene1_id = overlap[3]
+    gene2_id = overlap[7]
+    if gene1_id != gene2_id:
+      overlapping_genes.add(gene1_id)
+      overlapping_genes.add(gene2_id)
+
+  # filter for nonoverlapping genes
+  genes_bed_out = open(genes_bed_file, 'w')
+  for line in open(agenes_bed_file):
+    gene_id = line.split()[-1]
+    if gene_id not in overlapping_genes:
+      print(line, end='', file=genes_bed_out)
+  genes_bed_out.close()
+
+
+def make_genes_exon(genes_bed_file, genes_gtf_file, out_dir):
+  # read genes
+  genes_gtf = pygene.GTF(genes_gtf_file)
+
+  # write gene exons
+  agenes_bed_file = '%s/genes_all.bed' % out_dir
+  agenes_bed_out = open(agenes_bed_file, 'w')
+  for gene_id, gene in genes_gtf.genes.items():
+    # collect exons
+    gene_intervals = IntervalTree()
+    for tx_id, tx in gene.transcripts.items():
+      for exon in tx.exons:
+        gene_intervals[exon.start-1:exon.end] = True
+
+    # union
+    gene_intervals.merge_overlaps()
+
+    # write
+    for interval in sorted(gene_intervals):
+      cols = [gene.chrom, str(interval.begin), str(interval.end), gene_id, '.', gene.strand]
+      print('\t'.join(cols), file=agenes_bed_out)
+  agenes_bed_out.close()
+
+  # find overlapping exons
+  genes1_bt = pybedtools.BedTool(agenes_bed_file)
+  genes2_bt = pybedtools.BedTool(agenes_bed_file)
+  overlapping_exons = set()
+  for overlap in genes1_bt.intersect(genes2_bt, s=True, wo=True):
+    gene1_id = overlap[3]
+    gene1_start = int(overlap[1])
+    gene1_end = int(overlap[2])
+    overlapping_exons.add((gene1_id,gene1_start,gene1_end))
+      
+    gene2_id = overlap[9]
+    gene2_start = int(overlap[7])
+    gene2_end = int(overlap[8])
+    overlapping_exons.add((gene2_id,gene2_start,gene2_end))
+
+  # filter for nonoverlapping exons
+  genes_bed_out = open(genes_bed_file, 'w')
+  for line in open(agenes_bed_file):
+    a = line.split()
+    start = int(a[1])
+    end = int(a[2])
+    gene_id = a[-1]
+    if (gene_id,start,end) not in overlapping_exons:
+      print(line, end='', file=genes_bed_out)
+  genes_bed_out.close()
+
 
 ################################################################################
 # __main__
---
+++ bin/borzoi_test_genes_folds.py
@@ -0,0 +1,275 @@
+#!/usr/bin/env python
+# Copyright 2019 Calico LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =========================================================================
+from optparse import OptionParser, OptionGroup
+import glob
+import json
+import os
+import pdb
+import sys
+
+from natsort import natsorted
+import numpy as np
+import pandas as pd
+from scipy.stats import wilcoxon, ttest_rel
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import slurm
+
+"""
+basenji_test_folds.py
+
+Train Basenji model replicates using given parameters and data.
+"""
+
+################################################################################
+# main
+################################################################################
+def main():
+  usage = 'usage: %prog [options] <params_file> <data1_dir> ...'
+  parser = OptionParser(usage)
+  parser.add_option('-a', '--alt', dest='alternative',
+      default='two-sided', help='Statistical test alternative [Default: %default]')
+  parser.add_option('-c', dest='crosses',
+      default=1, type='int',
+      help='Number of cross-fold rounds [Default:%default]')
+  parser.add_option('-d', dest='dataset_i',
+      default=None, type='int',
+      help='Dataset index [Default:%default]')
+  parser.add_option('--d_ref', dest='dataset_ref_i',
+      default=None, type='int',
+      help='Reference Dataset index [Default:%default]')
+  parser.add_option('-e', dest='conda_env',
+      default='tf2.6',
+      help='Anaconda environment [Default: %default]')
+  parser.add_option('-f', dest='fold_subset',
+      default=None, type='int',
+      help='Run a subset of folds [Default:%default]')
+  parser.add_option('-g', dest='genes_gtf',
+      default='%s/genes/gencode38/gencode38_basic_protein.gtf' % os.environ['HG38'])
+  parser.add_option('--label_exp', dest='label_exp',
+      default='Experiment', help='Experiment label [Default: %default]')
+  parser.add_option('--label_ref', dest='label_ref',
+      default='Reference', help='Reference label [Default: %default]')
+  parser.add_option('-m', dest='metric',
+  	  default='PearsonR', help='Train/test metric [Default: Pearsonr or AUPRC]')
+  parser.add_option('--name', dest='name',
+      default='test', help='SLURM name prefix [Default: %default]')
+  parser.add_option('-o', dest='exp_dir',
+      default=None, help='Output experiment directory [Default: %default]')
+  parser.add_option('-p', dest='out_stem',
+      default=None, help='Output plot stem [Default: %default]')
+  parser.add_option('-q', dest='queue',
+      default='geforce')
+  parser.add_option('-r', dest='ref_dir',
+      default=None, help='Reference directory for statistical tests')
+  parser.add_option('--rc', dest='rc',
+      default=False, action='store_true',
+      help='Average forward and reverse complement predictions [Default: %default]')
+  parser.add_option('--shifts', dest='shifts',
+      default='0', type='str',
+      help='Ensemble prediction shifts [Default: %default]')
+  parser.add_option('--status', dest='status',
+      default=False, action='store_true',
+      help='Update metric status; do not run jobs [Default: %default]')
+  (options, args) = parser.parse_args()
+
+  if len(args) < 2:
+    parser.error('Must provide parameters file and data directory')
+  else:
+    params_file = args[0]
+    data_dirs = [os.path.abspath(arg) for arg in args[1:]]
+
+  # using -o for required argument for compatibility with the training script
+  assert(options.exp_dir is not None)
+
+  # read data parameters
+  data_stats_file = '%s/statistics.json' % data_dirs[0]
+  with open(data_stats_file) as data_stats_open:
+    data_stats = json.load(data_stats_open)
+
+  if options.dataset_i is None:
+    head_i = 0
+  else:
+    head_i = options.dataset_i
+
+  # count folds
+  num_folds = len([dkey for dkey in data_stats if dkey.startswith('fold')])
+
+  # subset folds
+  if options.fold_subset is not None:
+    num_folds = min(options.fold_subset, num_folds)
+
+  ################################################################
+  # test best
+  ################################################################
+  jobs = []
+
+  for ci in range(options.crosses):
+    for fi in range(num_folds):
+      it_dir = '%s/f%dc%d' % (options.exp_dir, fi, ci)
+
+      if options.dataset_i is None:
+        out_dir = '%s/testg' % it_dir
+        model_file = '%s/train/model_best.h5' % it_dir
+      else:
+        out_dir = '%s/testg%d' % (it_dir, options.dataset_i)
+        model_file = '%s/train/model%d_best.h5' % (it_dir, options.dataset_i)
+
+      # check if done
+      acc_file = '%s/acc.txt' % out_dir
+      if os.path.isfile(acc_file):
+        # print('%s already generated.' % acc_file)
+        pass
+      else:
+        # basenji test
+        cmd = '. /home/drk/anaconda3/etc/profile.d/conda.sh;'
+        cmd += ' conda activate %s;' % options.conda_env
+        cmd += ' borzoi_test_genes.py'
+        cmd += ' --head %d' % head_i
+        cmd += ' -o %s' % out_dir
+        if options.rc:
+          cmd += ' --rc'
+        if options.shifts:
+          cmd += ' --shifts %s' % options.shifts
+        cmd += ' %s' % params_file
+        cmd += ' %s' % model_file
+        cmd += ' %s/data%d' % (it_dir, head_i)
+        cmd += ' %s' % options.genes_gtf
+
+        name = '%s-testg-f%dc%d' % (options.name, fi, ci)
+        j = slurm.Job(cmd,
+                      name=name,
+                      out_file='%s.out'%out_dir,
+                      err_file='%s.err'%out_dir,
+                      queue=options.queue,
+                      cpu=2, gpu=1,
+                      mem=45000,
+                      time='6:00:00')
+        jobs.append(j)
+
+  slurm.multi_run(jobs, verbose=True)
+
+  if options.dataset_i is None:
+    test_prefix = 'test'
+  else:
+    test_prefix = 'test%d' % options.dataset_i
+
+  if options.dataset_ref_i is None:
+    test_ref_prefix = 'test'
+  else:
+    test_ref_prefix = 'test%d' % options.dataset_ref_i
+
+
+  ################################################################
+  # compare best on test set
+  ################################################################
+  exp_glob_str = '%s/*/%s/acc.txt' % (options.exp_dir, test_prefix)
+  exp_cors, exp_mean, exp_stdm = read_metrics(exp_glob_str, options.metric)
+
+  if options.ref_dir is not None:
+    ref_glob_str = '%s/*/%s/acc.txt' % (options.ref_dir, test_ref_prefix)
+    ref_cors, ref_mean, ref_stdm = read_metrics(ref_glob_str, options.metric)
+
+    mwp, tp = stat_tests(ref_cors, exp_cors, options.alternative)
+
+  print('\nTest (%d reps):' % len(exp_cors))
+  print('%12s %s: %.4f (%.4f)' % (options.label_exp, options.metric, exp_mean, exp_stdm))
+  if options.ref_dir is not None:
+    print('%12s %s: %.4f (%.4f)' % (options.label_ref, options.metric, ref_mean, ref_stdm))
+    print('Mann-Whitney U p-value: %.3g' % mwp)
+    print('T-test p-value: %.3g' % tp)
+
+    if options.out_stem is not None:
+      jointplot(ref_cors, exp_cors,
+          '%s_test.pdf' % options.out_stem,
+          options.label_ref, options.label_exp)
+
+
+def jointplot(ref_cors, exp_cors, out_pdf, label1, label2):
+  vmin = min(np.min(ref_cors), np.min(exp_cors))
+  vmax = max(np.max(ref_cors), np.max(exp_cors))
+  vspan = vmax - vmin
+  vbuf = vspan * 0.1
+  vmin -= vbuf
+  vmax += vbuf
+
+  g = sns.jointplot(ref_cors, exp_cors, space=0)
+
+  eps = 0.05
+  g.ax_joint.text(1-eps, eps, 'Mean: %.4f' % np.mean(ref_cors),
+    horizontalalignment='right', transform=g.ax_joint.transAxes)
+  g.ax_joint.text(eps, 1-eps, 'Mean: %.4f' % np.mean(exp_cors),
+    verticalalignment='top', transform=g.ax_joint.transAxes)
+
+  g.ax_joint.plot([vmin,vmax], [vmin,vmax], linestyle='--', color='orange')
+  g.ax_joint.set_xlabel(label1)
+  g.ax_joint.set_ylabel(label2)
+  
+  plt.tight_layout(w_pad=0, h_pad=0)
+  plt.savefig(out_pdf)
+
+
+def read_metrics(acc_glob_str, metric='pearsonr'):
+  rep_cors = []
+  acc_files = natsorted(glob.glob(acc_glob_str))
+  for acc_file in acc_files:
+    try:
+      # tf2 version
+      acc_df = pd.read_csv(acc_file, sep='\t', index_col=0)
+      rep_cors.append(acc_df.loc[:,metric].mean())
+
+    except:
+      # tf1 version
+      cors = []
+      for line in open(acc_file):
+        a = line.split()
+        cors.append(float(a[3]))
+      rep_cors.append(np.mean(cors))
+  
+  cors_mean = np.mean(rep_cors)
+  cors_stdm = np.std(rep_cors) / np.sqrt(len(rep_cors))
+
+  return rep_cors, cors_mean, cors_stdm
+
+
+def stat_tests(ref_cors, exp_cors, alternative):
+  # hack for the common situtation where I have more reference
+  # crosses than experiment crosses
+  if len(ref_cors) == 2*len(exp_cors):
+    ref_cors = [ref_cors[i] for i in range(len(ref_cors)) if i % 2 == 0]
+
+  _, mwp = wilcoxon(exp_cors, ref_cors, alternative=alternative)
+  tt, tp = ttest_rel(exp_cors, ref_cors)
+
+  if alternative == 'less':
+    if tt <= 0:
+      tp /= 2
+    else:
+      tp = 1 - (1-tp)/2
+  elif alternative == 'greater':
+    if tt >= 0:
+      tp /= 2
+    else:
+      tp = 1 - (1-tp)/2
+
+  return mwp, tp
+
+################################################################################
+# __main__
+################################################################################
+if __name__ == '__main__':
+  main()
